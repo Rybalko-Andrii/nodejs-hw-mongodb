869723c1932c17e06fdb01df71bfe267d3e9e6ea
Rybalko Andrii <0andryha0@gmail.com>
2025-06-14 09:25:33 +0200

    Add hw-3

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-export const initMongoConnections = async () => {
+export const initMongoDB = async () => {
   try {
     const user = getEnvVar('MONGODB_USER');
     const pwd = getEnvVar('MONGODB_PASSWORD');
@@ -9,13 +9,15 @@ export const initMongoConnections = async () => {
     const db = getEnvVar('MONGO_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Contacts`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
     console.log(
       'Mongo connection successfully established!',
     );
   } catch (e) {
-    console.log('Error');
+    console.log(
+      'Error while setting up mong connection',
+    );
     throw e;
   }
 };
